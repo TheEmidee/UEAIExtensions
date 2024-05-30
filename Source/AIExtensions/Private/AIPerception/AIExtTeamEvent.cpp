26a86660d17ef626971fe92a8ca054aab56a201b
Dimitri Paccagnini <dimitri.dimitri@fishingcactus.com>
2024-05-30 20:28:08 +0200

    AIPerception Team Sense blueprint exposed function

---
+++ Source/AIExtensions/Private/AIPerception/AIExtTeamEvent.cpp
@@ -0,0 +1,14 @@
+﻿#include "AIPerception/AIExtTeamEvent.h"
+
+#include "Perception/AIPerceptionSystem.h"
+#include "Perception/AISense_Team.h"
+
+void UAIExtTeamEvent::ReportTeamEvent( UObject * WorldContextObject, AActor * InBroadcaster, AActor * InEnemy, const FVector & InLastKnowLocation, float EventRange, float PassedInfoAge, float InStrength )
+{
+    UAIPerceptionSystem * PerceptionSystem = UAIPerceptionSystem::GetCurrent( WorldContextObject );
+    if ( PerceptionSystem )
+    {
+        FAITeamStimulusEvent Event( InBroadcaster, InEnemy, InLastKnowLocation, EventRange, PassedInfoAge, InStrength );
+        PerceptionSystem->OnEvent( Event );
+    }
+}
---
+++ Source/AIExtensions/Public/AIPerception/AIExtTeamEvent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UObject/ObjectMacros.h"
+
+#include "AIExtTeamEvent.generated.h"
+
+UCLASS()
+class AIEXTENSIONS_API UAIExtTeamEvent final : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+public:
+    UFUNCTION( BlueprintCallable, Category = "AI|Perception", meta = ( WorldContext = "WorldContextObject" ) )
+    static void ReportTeamEvent( UObject * WorldContextObject, AActor * InBroadcaster, AActor * InEnemy, const FVector & InLastKnowLocation, float EventRange, float PassedInfoAge, float InStrength );
+};
