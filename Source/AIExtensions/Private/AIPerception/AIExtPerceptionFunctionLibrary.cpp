355300e28ff8b8ab5337b8a6270ed8fafa375f79
Dimitri.Paccagnini <omygoditsdimi@gmail.com>
2024-05-31 16:22:43 +0200

    class name changed

--- Source/AIExtensions/Private/AIPerception/AIExtPerceptionFunctionLibrary.cpp
+++ Source/AIExtensions/Private/AIPerception/AIExtPerceptionFunctionLibrary.cpp
@@ -1,9 +1,9 @@
-﻿#include "AIPerception/AIExtTeamEvent.h"
+﻿#include "AIPerception/AIExtPerceptionFunctionLibrary.h"
 
 #include <Perception/AIPerceptionSystem.h>
 #include <Perception/AISense_Team.h>
 
-void UAIExtTeamEvent::ReportTeamEvent( UObject * world_context_object, AActor * broadcaster, AActor * enemy, const FVector & last_know_location, float event_range, float passed_info_age, float strength )
+void UAIExtPerceptionFunctionLibrary::ReportTeamEvent( UObject * world_context_object, AActor * broadcaster, AActor * enemy, const FVector & last_know_location, float event_range, float passed_info_age, float strength )
 {
     if ( auto * perception_system = UAIPerceptionSystem::GetCurrent( world_context_object ) )
     {
--- Source/AIExtensions/Public/AIPerception/AIExtPerceptionFunctionLibrary.h
+++ Source/AIExtensions/Public/AIPerception/AIExtPerceptionFunctionLibrary.h
@@ -4,13 +4,13 @@
 #include <Kismet/BlueprintFunctionLibrary.h>
 #include <UObject/ObjectMacros.h>
 
-#include "AIExtTeamEvent.generated.h"
+#include "AIExtPerceptionFunctionLibrary.generated.h"
 
 UCLASS()
-class AIEXTENSIONS_API UAIExtTeamEvent final : public UBlueprintFunctionLibrary
+class AIEXTENSIONS_API UAIExtPerceptionFunctionLibrary final : public UBlueprintFunctionLibrary
 {
     GENERATED_BODY()
 public:
-    UFUNCTION( BlueprintCallable, Category = "AI|Perception", meta = ( WorldContext = "WorldContextObject" ) )
+    UFUNCTION( BlueprintCallable, Category = "AI|Perception", meta = ( WorldContext = "world_context_object" ) )
     static void ReportTeamEvent( UObject * world_context_object, AActor * broadcaster, AActor * enemy, const FVector & last_know_location, float event_range, float passed_info_age, float strength );
 };
