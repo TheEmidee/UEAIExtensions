a018a89bdd63c53a80a6f4c63ace6fd576ff21da
Dimitri.Paccagnini <omygoditsdimi@gmail.com>
2024-05-31 13:49:59 +0200

    Review fixes - in prefixes

--- Source/AIExtensions/Private/AIPerception/AIExtTeamEvent.cpp
+++ Source/AIExtensions/Private/AIPerception/AIExtTeamEvent.cpp
@@ -3,11 +3,11 @@
 #include <Perception/AIPerceptionSystem.h>
 #include <Perception/AISense_Team.h>
 
-void UAIExtTeamEvent::ReportTeamEvent( UObject * world_context_object, AActor * in_broadcaster, AActor * in_enemy, const FVector & in_last_know_location, float event_range, float passed_info_age, float in_strength )
+void UAIExtTeamEvent::ReportTeamEvent( UObject * world_context_object, AActor * broadcaster, AActor * enemy, const FVector & last_know_location, float event_range, float passed_info_age, float strength )
 {
     if ( auto * perception_system = UAIPerceptionSystem::GetCurrent( world_context_object ) )
     {
-        FAITeamStimulusEvent Event( in_broadcaster, in_enemy, in_last_know_location, event_range, passed_info_age, in_strength );
+        FAITeamStimulusEvent Event( broadcaster, enemy, last_know_location, event_range, passed_info_age, strength );
         perception_system->OnEvent( Event );
     }
 }
--- Source/AIExtensions/Public/AIPerception/AIExtTeamEvent.h
+++ Source/AIExtensions/Public/AIPerception/AIExtTeamEvent.h
@@ -12,5 +12,5 @@ class AIEXTENSIONS_API UAIExtTeamEvent final : public UBlueprintFunctionLibrary
     GENERATED_BODY()
 public:
     UFUNCTION( BlueprintCallable, Category = "AI|Perception", meta = ( WorldContext = "WorldContextObject" ) )
-    static void ReportTeamEvent( UObject * world_context_object, AActor * in_broadcaster, AActor * in_enemy, const FVector & in_last_know_location, float event_range, float passed_info_age, float in_strength );
+    static void ReportTeamEvent( UObject * world_context_object, AActor * broadcaster, AActor * enemy, const FVector & last_know_location, float event_range, float passed_info_age, float strength );
 };
