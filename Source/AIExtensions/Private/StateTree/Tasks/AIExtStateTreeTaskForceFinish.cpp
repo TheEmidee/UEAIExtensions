aba43cca9fe7c36c797bd6439803003cd3d89bb3
Michael Delva <michael@emidee.net>
2024-11-25 17:58:08 +0100

    Created ST task to force finish the state

---
+++ Source/AIExtensions/Private/StateTree/Tasks/AIExtStateTreeTaskForceFinish.cpp
@@ -0,0 +1,12 @@
+﻿#include "StateTree/Tasks/AIExtStateTreeTaskForceFinishState.h"
+
+#include <StateTreeExecutionContext.h>
+
+EStateTreeRunStatus FAIExtStateTreeTaskForceFinish::EnterState( FStateTreeExecutionContext & context, const FStateTreeTransitionResult & transition ) const
+{
+    auto & instance_data = context.GetInstanceData( *this );
+
+    return instance_data.bSucceeded
+               ? EStateTreeRunStatus::Succeeded
+               : EStateTreeRunStatus::Failed;
+}
---
+++ Source/AIExtensions/Public/StateTree/Tasks/AIExtStateTreeTaskForceFinishState.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include <StateTreeTaskBase.h>
+
+#include "AIExtStateTreeTaskForceFinishState.generated.h"
+
+enum class EStateTreeRunStatus : uint8;
+struct FStateTreeTransitionResult;
+
+USTRUCT()
+struct AIEXTENSIONS_API FAIExtStateTreeTaskForceFinishInstanceData
+{
+    GENERATED_BODY()
+
+    FAIExtStateTreeTaskForceFinishInstanceData() = default;
+
+    /** Should the state finish with success?. */
+    UPROPERTY( EditAnywhere, Category = "Parameter" )
+    bool bSucceeded = true;
+};
+
+/**
+ * Task to force the state to finish
+ */
+USTRUCT( meta = ( DisplayName = "Force Finish State", Category = "State Tree Tasks|Helpers" ) )
+struct AIEXTENSIONS_API FAIExtStateTreeTaskForceFinish : public FStateTreeTaskCommonBase
+{
+    GENERATED_BODY()
+
+    using FInstanceDataType = FAIExtStateTreeTaskForceFinishInstanceData;
+
+    FAIExtStateTreeTaskForceFinish() = default;
+
+    const UStruct * GetInstanceDataType() const override;
+
+    EStateTreeRunStatus EnterState( FStateTreeExecutionContext & context, const FStateTreeTransitionResult & transition ) const override;
+};
+
+FORCEINLINE const UStruct * FAIExtStateTreeTaskForceFinish::GetInstanceDataType() const
+{
+    return FInstanceDataType::StaticStruct();
+}
