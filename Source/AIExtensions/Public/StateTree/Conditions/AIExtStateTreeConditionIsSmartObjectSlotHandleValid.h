2355b470b0cd181a062400fa0b0ab47f5dd31003
Michael Delva <michael@emidee.net>
2024-12-11 21:47:19 +0100

    Added ST task to enable a SO slot and a ST condition to check if a SO claimed slot handle is valid

---
+++ Source/AIExtensions/Public/StateTree/Conditions/AIExtStateTreeConditionIsSmartObjectSlotHandleValid.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include <SmartObjectTypes.h>
+#include <StateTreeConditionBase.h>
+
+#include "AIExtStateTreeConditionIsSmartObjectSlotHandleValid.generated.h"
+
+USTRUCT()
+struct FAIExtStateTreeConditionIsSmartObjectSlotHandleValidInstanceData
+{
+    GENERATED_BODY()
+
+    UPROPERTY( EditAnywhere, Category = "Input" )
+    FSmartObjectSlotHandle Slot;
+};
+
+/**
+ * Condition to check if a Smart Object slot handle is valid.
+ */
+USTRUCT( DisplayName = "Is Slot Handle Valid", Category = "Smart Object" )
+struct FAIExtStateTreeConditionIsSmartObjectSlotHandleValid : public FStateTreeConditionCommonBase
+{
+    GENERATED_BODY()
+
+    using FInstanceDataType = FAIExtStateTreeConditionIsSmartObjectSlotHandleValidInstanceData;
+
+    FAIExtStateTreeConditionIsSmartObjectSlotHandleValid() = default;
+
+    bool Link( FStateTreeLinker & linker ) override;
+
+    const UStruct * GetInstanceDataType() const override
+    {
+        return FInstanceDataType::StaticStruct();
+    }
+
+    bool TestCondition( FStateTreeExecutionContext & context ) const override;
+
+protected:
+    UPROPERTY( EditAnywhere, Category = "Condition" )
+    bool bInvert = false;
+
+    /** Handle to retrieve USmartObjectSubsystem. */
+    TStateTreeExternalDataHandle< USmartObjectSubsystem > SmartObjectSubsystemHandle;
+};
