343fdffcfb7d6e15cbf1902009948652f3117fda
michael.delva <michael.delva@fishingcactus.com>
2025-02-27 18:22:19 +0100

    Removed dependency to StructUtils

--- Source/AIExtensions/AIExtensions.Build.cs
+++ Source/AIExtensions/AIExtensions.Build.cs
@@ -22,7 +22,6 @@ namespace UnrealBuildTool.Rules
 					"GameplayTags",
 					"StateTreeModule",
 					"NavigationSystem",
-					"StructUtils", 
 					"SmartObjectsModule",
 					"GameplayStateTreeModule"
 				}
