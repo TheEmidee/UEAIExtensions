8094d1656c9c95a6b29ba7b9e499efe5f6ae9b7e
Michael Delva <michael@emidee.net>
2025-04-09 20:05:18 +0200

    Moved Smart Objects related tasks in a dedicated folder

---
+++ Source/AIExtensions/Private/StateTree/Tasks/SmartObjects/AIExtStateTreeTaskSmartObjectFreeSlot.cpp
@@ -0,0 +1,43 @@
+﻿#include "StateTree/Tasks/SmartObjects/AIExtStateTreeTaskSmartObjectFreeSlot.h"
+
+#include <SmartObjectSubsystem.h>
+#include <StateTreeExecutionContext.h>
+#include <StateTreeLinker.h>
+#include <VisualLogger/VisualLogger.h>
+
+FAIExtStateTreeTaskSmartObjectFreeSlot::FAIExtStateTreeTaskSmartObjectFreeSlot()
+{
+    // No tick needed.
+    bShouldCallTick = false;
+    // No need to update bound properties after enter state.
+    bShouldCopyBoundPropertiesOnTick = false;
+    bShouldCopyBoundPropertiesOnExitState = false;
+}
+
+bool FAIExtStateTreeTaskSmartObjectFreeSlot::Link( FStateTreeLinker & linker )
+{
+    linker.LinkExternalData( SmartObjectSubsystemHandle );
+    return true;
+}
+
+inline EStateTreeRunStatus FAIExtStateTreeTaskSmartObjectFreeSlot::EnterState( FStateTreeExecutionContext & context, const FStateTreeTransitionResult & transition ) const
+{
+    TRACE_CPUPROFILER_EVENT_SCOPE_STR( __FUNCTION__ );
+
+    auto & smart_object_subsystem = context.GetExternalData( SmartObjectSubsystemHandle );
+    const auto & instance_data = context.GetInstanceData< FInstanceDataType >( *this );
+
+    if ( !instance_data.ClaimHandle.IsValid() )
+    {
+        UE_VLOG( context.GetOwner(), LogStateTree, Error, TEXT( "FAIExtStateTreeTaskSmartObjectFreeSlot failed because the claim handle is invalid." ) );
+        return EStateTreeRunStatus::Failed;
+    }
+
+    if ( smart_object_subsystem.MarkSlotAsFree( instance_data.ClaimHandle ) )
+    {
+        return EStateTreeRunStatus::Running;
+    }
+
+    UE_VLOG( context.GetOwner(), LogStateTree, Error, TEXT( "FAIExtStateTreeTaskSmartObjectFreeSlot failed because an error occured while freeing the claim handle." ) );
+    return EStateTreeRunStatus::Failed;
+}
---
+++ Source/AIExtensions/Private/StateTree/Tasks/SmartObjects/AIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation.cpp
@@ -0,0 +1,42 @@
+﻿#include "StateTree/Tasks/SmartObjects/AIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation.h"
+
+#include <SmartObjectSubsystem.h>
+#include <StateTreeExecutionContext.h>
+#include <StateTreeLinker.h>
+#include <VisualLogger/VisualLogger.h>
+
+FAIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation::FAIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation()
+{
+    // No tick needed.
+    bShouldCallTick = false;
+    // No need to update bound properties after enter state.
+    bShouldCopyBoundPropertiesOnTick = false;
+    bShouldCopyBoundPropertiesOnExitState = false;
+}
+
+bool FAIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation::Link( FStateTreeLinker & linker )
+{
+    linker.LinkExternalData( SmartObjectSubsystemHandle );
+    return true;
+}
+
+inline EStateTreeRunStatus FAIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation::EnterState( FStateTreeExecutionContext & context, const FStateTreeTransitionResult & transition ) const
+{
+    TRACE_CPUPROFILER_EVENT_SCOPE_STR( __FUNCTION__ );
+
+    auto & smart_object_subsystem = context.GetExternalData( SmartObjectSubsystemHandle );
+    auto & instance_data = context.GetInstanceData< FInstanceDataType >( *this );
+
+    if ( !instance_data.SlotHandle.IsValid() )
+    {
+        UE_VLOG( context.GetOwner(), LogStateTree, Error, TEXT( "FAIExtStateTreeTaskSmartObjectGetSlotLocationAndRotation failed because the slot handle is invalid." ) );
+        return EStateTreeRunStatus::Failed;
+    }
+
+    const auto transform = smart_object_subsystem.GetSlotTransformChecked( instance_data.SlotHandle );
+    instance_data.SlotLocation = transform.GetLocation();
+    instance_data.SlotRotationQuat = transform.GetRotation();
+    instance_data.SlotRotation = instance_data.SlotRotationQuat.Rotator();
+
+    return EStateTreeRunStatus::Running;
+}
